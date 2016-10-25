6b5567bd4c23bb94c781dde8c3d9fb3165d8155b
Matthias <mail@matthiasbock.net>
2016-10-25 19:32:45 +0200

    added C header file for nRF51 firmware development

---
+++ pinout.h
@@ -0,0 +1,18 @@
+/**
+ * C header file implementing the
+ * nRF51 GPIO pinout of this PCB
+ */
+
+#ifndef CORE51822_ICESTICK_PINOUT_H
+#define CORE51822_ICESTICK_PINOUT_H
+
+#define ADAPTER_PIN_3   29
+#define ADAPTER_PIN_4   15
+#define ADAPTER_PIN_5   13
+#define ADAPTER_PIN_6   11
+#define ADAPTER_PIN_7    9
+#define ADAPTER_PIN_8    7
+#define ADAPTER_PIN_9    5
+#define ADAPTER_PIN_10   6
+
+#endif // CORE51822_ICESTICK_PINOUT_H
